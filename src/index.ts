74e8faeca11181de4e808a5c0fa9e73f36af9e07
codewaseem <codewaseem@gmail.com>
2023-02-18 10:32:02 +0530

    current changes

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,25 @@
 import { data } from "./data";
 
-const columnsCount = 20;
-const WIDTH_ADJUST = 10;
-const COLUMN_WIDTH  = 25;
+const cols = data.reduce((prev, cur) => {
+  const key = +cur.center_ch;
+  if(!prev[key]) prev[key] = {
+    count: 0,
+    color: Math.floor(Math.random()*16777215).toString(16)
+  };
+  
+  prev[key].count++;
 
-const xOffset = 200;
+  return prev;
+}, {} as LineData);
+
+const colKeys = Object.keys(cols).sort((a, b) => +a - +b);
+const columnsCount = colKeys.length;
+
+
+const WIDTH_ADJUST = 4;
+const COLUMN_WIDTH  = 600 / columnsCount;
+
+const xOffset = 0;
 
 
 
@@ -59,31 +74,13 @@ const series: Highcharts.SeriesOptionsType[] = [];
 
 // add columns
 
-// Array(2).fill(100).forEach((columnY, i) => {
-//   series.push({
-//     type: 'column',
-//     color: '#CCC3',
-//     data: [{
-//       x: xOffset - WIDTH_ADJUST * (i + 1),
-//       y: columnY,
-//       dataLabels: {
-//         enabled: false
-//       }
-//     }]
-//   });
-// });
 
-const cols = data.reduce((prev, cur) => {
-  if(!prev[cur.center_ch]) prev[cur.center_ch] = 0;
-  
-  prev[cur.center_ch]++;
+type LineData = Record<number, {
+  count: number,
+  color: string,
+  center?: number
+}>;
 
-  return prev;
-}, {} as Record<string, number>);
-
-const centers: Record<string, number> = {};
-
-const colKeys = Object.keys(cols);
 
 colKeys.forEach((key, i) => {
 
@@ -119,7 +116,7 @@ colKeys.forEach((key, i) => {
     }]
   });
 
-  centers[key] = xOffset +  i * WIDTH_ADJUST;
+  cols[+key].center =  xOffset +  i * WIDTH_ADJUST;
   
 
   if(i == (colKeys.length - 1)) {
@@ -142,16 +139,17 @@ colKeys.forEach((key, i) => {
 
 // add channel
 data.slice(0, 10).forEach((item) => {
-  const center = centers[item.center_ch];
+  const center = cols[+item.center_ch].center || Number(item.center_ch) * WIDTH_ADJUST - WIDTH_ADJUST;
   const width = Number(item.width);
   const height = Math.abs(Number(item.signal_strength));
 
   const point1 = [center - width , 0];
-  const point2 =  [center - width / 2, height];
+  const point2 =  [center - width / 2 , height];
   const point3 = [center + width / 2 , height];
-  const point4 = [center + width, 0]
+  const point4 = [center + width , 0]
   series.push({
     type: "line",
+    color: '#' + cols[+item.center_ch].color,
     data: [
       point1,
       point2,
@@ -164,83 +162,3 @@ data.slice(0, 10).forEach((item) => {
 renderChart(series);
 
 
-// const chart = Highcharts.chart("container", {
-//   title: {
-//     text: "Monthly Average Rainfall",
-//   },
-
-//   xAxis: {
-//     min: 0,
-//     lineWidth: 0,
-//     tickInterval: 10,
-//     minorGridLineWidth: 0,
-//     lineColor: "transparent",
-//     minorTickLength: 0,
-//   },
-//   yAxis: {
-//     min: 0,
-//     max: 100,
-//     tickInterval: 10,
-//     lineWidth: 0,
-//     minorGridLineWidth: 0,
-//     lineColor: "transparent",
-
-//     minorTickLength: 0,
-//     tickLength: 0,
-//   },
-//   tooltip: {
-//     enabled: false,
-//   },
-//   plotOptions: {
-//     column: {
-//       pointWidth: 30,
-//       centerInCategory: true,
-//       showInLegend: false,
-//     },
-//     line: {
-//       showInLegend: false,
-//       marker: {
-//         enabled: false,
-//         radius: 0,
-//         states: {
-//           hover: {
-//             radius: 0,
-//           },
-//         },
-//       },
-//     },
-//   },
-//   series: [
-//     ...Array(columnsCount)
-//       .fill(100)
-//       .map((column, i) => {
-//         return {
-//           type: "column",
-//           color: "#0F03",
-//           data: [
-//             {
-//               x: i * 10,
-//               y: column,
-//               dataLabels: {
-//                 enabled: true,
-//                 formatter: () => {
-//                   return `Ch ${i + 1}`;
-//                 },
-//               },
-//             },
-//           ],
-//         };
-//       }),
-//     ...hrLines.map((line) => {
-//       return {
-//         type: "line",
-//         data: [
-//           [line.center - line.width + 5 + columnOffset, 0],
-//           [line.center - line.width / 2 + columnOffset, line.height],
-//           [line.center + line.width / 2 + columnOffset, line.height],
-//           [line.center + line.width - 5 + columnOffset, 0],
-//         ],
-//       };
-//     }),
-//   ],
-// });
